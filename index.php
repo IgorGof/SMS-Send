7af80d0f2a0cd5ed94309325b6dd6e64cce60595
Goferberg Igor <igorgof@yandex.ru>
2018-04-18 08:09:07 +0500

    fix upload xml files
    Теперь загружать можно только xml файлы

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
     <h1>Отправка СМС</h1>
     <input type="hidden" name="MAX_FILE_SIZE" value="30000" />
     <form action="send.php" method="POST" enctype="multipart/form-data">
-      <input type="file" name="filename"/>
+      <input type="file" name="filename" accept="text/xml"/>
       <input name="MySend" type="submit" value="Отправить" />
     </form>
     </div>
